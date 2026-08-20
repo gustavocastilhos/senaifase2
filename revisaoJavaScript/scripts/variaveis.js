7bd4da0cb47dc119498db007b748317b27b580ef
gustavocastilhos <gusmarcast@gmail.com>
2026-08-19 21:06:17 -0300

    Testando servidor

--- revisaoJavaScript/scripts/variaveis.js
+++ revisaoJavaScript/scripts/variaveis.js
@@ -1,5 +1,12 @@
 function exemplosVariaveis(){
-console.log("Ola mundo!");
+console.log("Variaveis rodando...");
+
+const nome = 'João Silva';  // String
+const idade = 18; // Number
+const possuiFaculdade =  true; // Boolean
+const time = "Brasil" // Undefined
+const comida = null; // Null
+const novoObjeto = {} // Object
 
 }
 
