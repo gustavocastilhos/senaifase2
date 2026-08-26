c138b63ee857ad5cc30040441429f625a7533627
gustavocastilhos <gusmarcast@gmail.com>
2026-08-26 19:31:35 -0300

    Feat

--- revisaoJavaScript/main.js
+++ revisaoJavaScript/main.js
@@ -1,9 +1,14 @@
 
 import exemplosVariaveis from "./scripts/variaveis.js";
+import exemplosCondicionais from "./scripts/outros/condicionais.js";
 
 exemplosVariaveis()
 
 
 
+
 const btnVartiaveis = document.getElementById("btnVariaveis");
 btnVartiaveis.addEventListener("click", exemplosVariaveis)
+
+
+document.getElementById("btnCondicionais").addEventListener("click", exemplosCondicionais);
